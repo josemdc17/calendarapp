180466ad9a86eeb25156440e6075cd2145b7bd0d
Testing JM <77297786+jmtesting1792@users.noreply.github.com>
2023-02-01 00:13:38 -0500

    now it can highlight the present day

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -44,14 +44,19 @@ const Calendar = () => {
   for (let i = 0; i < (days + totalOfEmptyDays); i++) {
     if (i < totalOfEmptyDays) {
       listDays.push(<div key={i}></div>);
-    }else if(i === day){
-      listDays.push(<div className="days-item" id="presentDay" key={i}>{j + 1}</div>);
-      j++;
     } else {
-      listDays.push(<div className="days-item" key={i}>{j + 1}</div>);
-      j++;
+      if(i  === day + totalOfEmptyDays && value === month){
+        listDays.push(<div className="days-item" id="presentDay" key={i}>{j + 1}</div>);
+        j++;
+      } else {
+        listDays.push(<div className="days-item" key={i}>{j + 1}</div>);
+        j++;
+      }
+      
     }
   }
+
+  console.log(day)
 
   return (
     <div className="calendarWrapper">
